301559c3b70e6530dce7d5e6108e90e927ca8575
Rômulo Zirbes <rzirbes5@rede.ulbra.br>
2022-11-19 16:38:18 -0300

    adicionado autenticação de email e senha

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -9,7 +9,16 @@ import 'cart.dart';
 import 'order.dart';
 
 class OrderList with ChangeNotifier {
-  final List<Order> _items = [];
+  final String _token;
+  final String _userId;
+  // ignore: prefer_final_fields
+  List<Order> _items = [];
+
+  OrderList([
+    this._token = '',
+    this._userId = '',
+    this._items = const [],
+  ]);
 
   List<Order> get items {
     return [..._items];
@@ -20,16 +29,16 @@ class OrderList with ChangeNotifier {
   }
 
   Future<void> loadOrders() async {
-    _items.clear();
+    List<Order> items = [];
 
     final response = await http.get(
-      Uri.parse('${Constants.orderBaseUrl}.json'),
+      Uri.parse('${Constants.orderBaseUrl}/$_userId.json?auth=$_token'),
     );
     if (response.body == 'null') return;
     Map<String, dynamic> data = jsonDecode(response.body);
     data.forEach(
       (orderId, orderData) {
-        _items.add(
+        items.add(
           Order(
             id: orderId,
             date: DateTime.parse(orderData['date']),
@@ -47,13 +56,15 @@ class OrderList with ChangeNotifier {
         );
       },
     );
+
+    _items = items.reversed.toList();
     notifyListeners();
   }
 
   Future<void> addOrder(Cart cart) async {
     final date = DateTime.now();
     final response = await http.post(
-      Uri.parse('${Constants.orderBaseUrl}.json'),
+      Uri.parse('${Constants.orderBaseUrl}/$_userId.json?auth=$_token'),
       body: jsonEncode(
         {
           'total': cart.totalAmount,
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,5 @@
 class Constants {
+  static const userFavoritesUrl = 'https://shop-cod3r-dae21-default-rtdb.firebaseio.com/userFavorite';
   static const productBaseUrl = 'https://shop-cod3r-dae21-default-rtdb.firebaseio.com/products';
   static const orderBaseUrl = 'https://shop-cod3r-dae21-default-rtdb.firebaseio.com/orders';
 
